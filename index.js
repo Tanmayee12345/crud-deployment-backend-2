b686154e457e1518a7d637abb3148c7faf2b95d5
Tanmayee12345 <tanutanmayee2003@gmail.com>
2023-10-27 17:28:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const mongoose=require("mongoose");
+const express=require("express");
+const studentRoute = require("./controller/studentRoute");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app=express();
+
+mongoose.set("strictQuery",true);
+mongoose.connect("mongodb+srv://test:tan12345@cluster0.zh4wfih.mongodb.net/schooldb");
+var db=mongoose.connection;
+db.on("open",()=>console.log("connected to db"));
+db.on("error",()=>console.log("error occured"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+
+app.use("/studentRoute",studentRoute);
+app.listen(4000,()=>console.log("server started at 4000"));
